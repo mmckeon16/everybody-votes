4599da47a4aa2015b942a47ef45221ab78c70e37
ngattusohw <ngattusohw@gmail.com>
2024-12-08 00:52:31 -0500

    Fixing a ton of bugs and making the app work again

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,17 @@
-import { AuthProvider } from '../context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <Stack>
-        <Stack.Screen name="(app)" options={{ headerShown: false }} />
-        <Stack.Screen name="auth" options={{ headerShown: false }} />
-      </Stack>
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <Stack>
+          <Stack.Screen name="(app)" options={{ headerShown: false }} />
+          <Stack.Screen name="auth" options={{ headerShown: false }} />
+        </Stack>
+      </AuthProvider>
+    </Provider>
   );
 }
