9358577a2c76f35a55e6d0a76ad6451d12f2404e
mmckeon16 <mmckeon204@gmail.com>
2024-12-13 23:41:57 -0500

    fixing result query

--- app/screens/results/index.tsx
+++ app/screens/results/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { View, ScrollView, ActivityIndicator } from 'react-native';
 import { useGetResultsForQuestionQuery } from '../../store/api/resultsApi';
-
-import { Stack } from 'expo-router';
-import { Question } from '../../types';
-import QuestionResults from '../../components/QuestionResults';
-import ThemedText from '../../components/ThemedText';
+import { Text } from '~/components/ui/text';
 import PieChart from '../../components/PieChart';
+import { aggregateVotes } from '../../lib/utils';
 
 export default function Results() {
   const { data: results, isLoading, error } = useGetResultsForQuestionQuery({
@@ -32,15 +29,63 @@ export default function Results() {
   //   );
   // }
 
+  /*
+  id,
+  created_at,
+  options (
+    id,
+    text,
+    question_id,
+    questions (
+      id,
+      text
+
+      */
+
+  const rawData = [
+    {
+      id: 'UUID HERE',
+      created_at: 'new Date',
+      options: {
+        id: 'option id',
+        text: 'Yes',
+        question_id: 'your-question-id',
+        questions: {
+          id: 'your-question-id',
+          text: 'Should US Presidents have term limits',
+        },
+      },
+    },
+    {
+      id: 'UUID HERE',
+      created_at: 'new Date',
+      options: {
+        id: 'option id 2',
+        text: 'No',
+        question_id: 'your-question-id',
+        questions: {
+          id: 'your-question-id',
+          text: 'Should US Presidents have term limits',
+        },
+      },
+    },
+  ];
+  const aggregateData = aggregateVotes(rawData);
+  console.log('aggregateData: ', aggregateData);
+
   const data = [
-    { label: 'Option A', value: 50, color: '#56a5f5' },
-    { label: 'Option B', value: 50, color: '#eb8f49' },
+    { id: 'ksdjp', text: 'Option A', percentage: 50, color: '#56a5f5' },
+    { id: 'hjnv', text: 'Option B', percentage: 50, color: '#eb8f49' },
   ];
 
+  // TODO add in prediction
   return (
-    <View className="flex-1">
+    <View className="flex-column items-center overflow-hidden flex-1">
+      <Text className="text-2xl text-inherit p-2.5">
+        {aggregateData.questionText}
+      </Text>
       <ScrollView className="items-center flex-1 p-6">
-        <PieChart data={data} size={200} strokeWidth={25} />
+        <PieChart data={aggregateData.options} size={200} strokeWidth={25} />
       </ScrollView>
     </View>
   );
