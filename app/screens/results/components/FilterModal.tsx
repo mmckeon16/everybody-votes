fa93aa99ef1c51387707a99a4fc3c6f0e00dd6e2
mmckeon16 <mmckeon204@gmail.com>
2024-12-24 12:02:03 -0500

    fixing styling for filter modal

--- app/screens/results/components/FilterModal.tsx
+++ app/screens/results/components/FilterModal.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Alert, Modal, ScrollView, View } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { Button } from '~/components/ui/button';
 import { Text } from '~/components/ui/text';
 import { Label } from '~/components/ui/label';
@@ -27,66 +28,76 @@ const FilterModal = () => {
           setModalVisible(!modalVisible);
         }}
       >
-        <ScrollView className="flex-1 items-center justify-center">
-          <View className="m-5 bg-white rounded-3xl p-8 items-center shadow-lg w-screen">
-            <Accordion
-              type="multiple"
-              collapsible
-              defaultValue={['item-1']}
-              className="w-full max-w-sm native:max-w-md"
-            >
-              {demographics.map(({ name, options }) => (
-                <AccordionItem value={name}>
-                  <AccordionTrigger>
-                    <Text>{name}</Text>
-                  </AccordionTrigger>
-                  <AccordionContent>
-                    <ScrollView
-                      className="max-h-52"
-                      contentContainerStyle={{
-                        flexDirection: 'row',
-                        flexWrap: 'wrap',
-                        gap: 8, // This will add spacing between items
-                        flexGrow: 1,
-                      }}
-                    >
-                      {name === 'Country of origin'
-                        ? options?.map(option => (
-                            <View className="flex flex-row gap-2 w-1/3 px-2">
-                              <Checkbox
-                                id={option}
-                                checked={false}
-                                onCheckedChange={() => {
-                                  console.log('checked', option);
-                                }}
-                              />
-                              <Label nativeID={option}>{option}</Label>
-                            </View>
-                          ))
-                        : options?.map(({ label, value }) => (
-                            <View className="flex flex-row gap-2 w-1/3 px-2">
-                              <Checkbox
-                                id={value}
-                                checked={false}
-                                onCheckedChange={() => {
-                                  console.log('checked', label);
-                                }}
-                              />
-                              <Label nativeID={value}>{label}</Label>
-                            </View>
-                          ))}
-                    </ScrollView>
-                  </AccordionContent>
-                </AccordionItem>
-              ))}
-            </Accordion>
-            <Button
-              variant="outline"
-              onPress={() => setModalVisible(!modalVisible)}
-              className="mt-6"
-            >
-              <Text>Generate visualization</Text>
-            </Button>
+        <ScrollView>
+          <View className="flex-1 items-center justify-center px-5 min-h-screen">
+            <View className="m-5 bg-white rounded-3xl p-8 items-center shadow-lg w-full">
+              <Button
+                size="icon"
+                variant="ghost"
+                className="self-end"
+                onPress={() => setModalVisible(false)}
+              >
+                <AntDesign name="close" size={24} />
+              </Button>
+              <Accordion
+                type="multiple"
+                collapsible
+                defaultValue={['item-1']}
+                className="w-full native:max-w-md"
+              >
+                {demographics.map(({ name, options }) => (
+                  <AccordionItem value={name}>
+                    <AccordionTrigger>
+                      <Text>{name}</Text>
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <ScrollView
+                        className="max-h-52"
+                        contentContainerStyle={{
+                          flexDirection: 'row',
+                          flexWrap: 'wrap',
+                          gap: 8, // This will add spacing between items
+                          flexGrow: 1,
+                        }}
+                      >
+                        {name === 'Country of origin'
+                          ? options?.map(option => (
+                              <View className="flex flex-row gap-2 w-2/5 px-2 truncate">
+                                <Checkbox
+                                  id={option}
+                                  checked={false}
+                                  onCheckedChange={() => {
+                                    console.log('checked', option);
+                                  }}
+                                />
+                                <Label nativeID={option}>{option}</Label>
+                              </View>
+                            ))
+                          : options?.map(({ label, value }) => (
+                              <View className="flex flex-row gap-2 w-1/3 px-2">
+                                <Checkbox
+                                  id={value}
+                                  checked={false}
+                                  onCheckedChange={() => {
+                                    console.log('checked', label);
+                                  }}
+                                />
+                                <Label nativeID={value}>{label}</Label>
+                              </View>
+                            ))}
+                      </ScrollView>
+                    </AccordionContent>
+                  </AccordionItem>
+                ))}
+              </Accordion>
+              <Button
+                variant="outline"
+                onPress={() => setModalVisible(!modalVisible)}
+                className="mt-6"
+              >
+                <Text>Generate visualization</Text>
+              </Button>
+            </View>
           </View>
         </ScrollView>
       </Modal>
